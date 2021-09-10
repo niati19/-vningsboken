1a9f9b5a09848e3afe9e83c22ece1671347e4d4e
niati19 <90190320+niati19@users.noreply.github.com>
2021-09-10 13:38:48 +0200

    🎨 edited stuff

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 print("HEEEEEEEEJ")
 print("print")
+
+name = input("Vad heter du? ")
+print(f"{name}, det var inte så fint namn")
+
+age = input("Hur gammal är du? ")
+print(f"{age} är väldigt dåligt nummer!")
+
+sure = input(f"Är du säker att du är {age}? ")
+print ("Tråkigt för dig")
