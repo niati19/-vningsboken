b32f16b2afafb575650b077ed391fe2cb920dd94
niati19 <90190320+niati19@users.noreply.github.com>
2021-10-01 13:37:56 +0200

    Bästa danskoden asså wow

---
+++ Dansa.py
@@ -0,0 +1,15 @@
+print("Nu fan ska vi dansa skitunge")
+print("Vad vill du göra?\n [1.] Helikoptern\n [2.] cringe nae nae\n [3.] Inget")
+svar = ""
+
+while svar != "cringe":
+    svar = input("Vad ska du göra: ")
+
+    if svar == "1":
+        print("Oj oj det var lite galet")
+    elif svar == "2":
+        print("Det var lite cringe grabben")
+    elif svar == "3":
+        print("Du suger hej då")
+        exit()
+    elif svar == "4"
